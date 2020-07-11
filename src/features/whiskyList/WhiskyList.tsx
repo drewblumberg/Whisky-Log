0dcd2c580c488e9cc6e9e6ec2a554156165d6143
Drew Blumberg <alblumberg21@gmail.com>
2020-07-11 17:13:21 -0500

    better list styling

--- src/features/whiskyList/WhiskyList.tsx
+++ src/features/whiskyList/WhiskyList.tsx
@@ -3,21 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import WhiskyListItem from './WhiskyListItem';
 import { RootState } from '../../store/rootReducer';
 import { loadWhiskys } from './WhiskyListReducer';
+import './WhiskyList.css';
 
 export default function WhiskyList() {
   const dispatch = useDispatch();
   const whiskys = useSelector(
-      (state: RootState) => state.whiskyList
+    (state: RootState) => state.whiskyList
   );
 
   React.useEffect(() => {
-    dispatch(loadWhiskys());
+    if (!whiskys.length) {
+      dispatch(loadWhiskys());
+    }
   });
 
   return (
-    <ul>
+    <ul className="whisky-list">
       {whiskys.map(whisky => (
-        <WhiskyListItem key={whisky.id} name={whisky.name} />
+        <WhiskyListItem key={whisky.id} name={whisky.name} image={whisky.image} proof={whisky.proof} type={whisky.type} />
       ))}
     </ul>
   );
--- src/features/whiskyList/WhiskyListItem.tsx
+++ src/features/whiskyList/WhiskyListItem.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './WhiskyList.css';
 
 interface WhiskyProps {
     name: string,
+    image: string,
+    type: string,
+    proof: number
 }
 
-export default function TodoListItem({ name }: WhiskyProps) {
+export default function TodoListItem({ name, image, proof, type }: WhiskyProps) {
     return (
-        <li>
-            {name}
+        <li className='whisky-item'>
+            <div className='whisky-item-image-container'>
+                <img alt={name + 'bottle picture'} className='whisky-item-image' src={image} />
+            </div>
+            <div className='whisky-item-info'>
+                <span className='whisky-item-name'>{name}</span>
+                <span className='whisky-item-other'>{type}</span>
+                <span className='whisky-item-other'>{proof} Proof</span>
+            </div>
         </li>
     );
 }
